13fbbee39c3175dbe31e8db9c1c69672317b33b5
Michael Norman <normanm5@cardiff.ac.uk>
2023-08-17 18:58:14 -0700

    Updated validation

--- plot_validation.py
+++ plot_validation.py
@@ -1,321 +1,40 @@
 import numpy as np
 
-from common_functions import *
-from matplotlib.collections import LineCollection
-import matplotlib
-
-from bokeh.embed import components
-
-from itertools import cycle
-from bokeh.resources import INLINE
-from bokeh.resources import Resources
-from bokeh.embed import file_html
-
-import h5py
-
-from bokeh.plotting import figure, show, save
-from bokeh.models import Legend, ColumnDataSource, LogAxis, Range1d, LogTicker, Slider, CustomJS, Dropdown
-from bokeh.io import export_png
-from bokeh.layouts import gridplot, column
-
-from bokeh.io import output_file, save
-from bokeh.models import HoverTool, Legend, LogAxis
-
-# Load data from HDF5 file
-def load_data_from_hdf5(model_name):
-    with h5py.File(f'./{model_name}_data.h5', 'r') as h5f:
-        # Load ROC data
-        roc_data = {
-            'fpr': h5f['roc_data']['fpr'][()],
-            'tpr': h5f['roc_data']['tpr'][()],
-            'roc_auc': h5f['roc_data'].attrs['roc_auc']
-        }
-
-        # Load efficiency scores
-        efficiency_scores = []
-        eff_group = h5f['efficiency_scores']
-        for i in range(len(eff_group.keys())):
-            efficiency_scores.append(eff_group[f'score_{i}'][()])
-
-        # Load FAR scores
-        far_scores = h5f['far_scores']['scores'][()]
-
-    return roc_data, efficiency_scores, far_scores
-
-def return_time_labels(total_num_seconds, seconds_in_time = None):
-        
-    if seconds_in_time == None:
-        seconds_in_time = {
-            315360000: "$Decade^{-1}$", 
-            31536000: "$Year^{-1}$",
-            2628288: "$Month^{-1}$", 
-            604800: "$Week^{-1}$", 
-            86400: "$Day^{-1}$", 
-            3600: "$Hour^{-1}$", 
-            60: "$Minute^{-1}$",
-            1: "$Second^{-1}$"
-        }
-    
-    y_ticks       = []
-    y_tick_labels = []
-    for time in seconds_in_time.keys():
-        if time < total_num_seconds:
-            y_ticks.append(1/time)
-            y_tick_labels.append(seconds_in_time[time])
-            
-    return y_ticks, y_tick_labels
-
-def calculate_far_score_thresholds(model_scores, model_data, fars, restriction):
-        
-    total_num_seconds = 0
-    
-    score_thresholds = {}
-    
-    for name in model_names:
-        far_scores        = data[name]["far_scores"]
-        far_scores        = np.sort(far_scores)[::-1]
-        total_num_seconds = len(far_scores)
-        far_axis          = (np.arange(total_num_seconds)+1)/total_num_seconds
-        
-        score_thresholds[name] = {}
-        
-        for far in fars:
-            idx = np.abs(far - far_axis).argmin()
-            idx = np.abs(far_scores - far_scores[idx]).argmin()
-            
-            score_thresholds[name][far] = (far, far_scores[idx])
-            
-            if (far_scores[idx] == 1):
-                 score_thresholds[name][far] = (far, 1.1)
-    
-    return score_thresholds
-
-def plot_far_curves(model_names, model_titles, model_data, fars, output_path):
-    plot_width = 800
-    plot_height = 600
-    tooltips = [
-        ("Name", "@name"),
-        ("Score Threshold", "@x"),
-        ("False Alarms per Second", "@y"),
-    ]
-
-    p = figure(
-        width=plot_width,
-        height=plot_height,
-        x_axis_label="Score Threshold",
-        y_axis_label="False Alarms per Second",
-        tooltips=tooltips,
-        x_axis_type="log",
-        y_axis_type="log"
-    )
-    
-    colors = cycle(['red', 'blue', 'green', 'orange'])
-
-    for color, (name, title) in zip(colors, zip(model_names, model_titles)):
-        far_scores = model_data[name]["far_scores"]
-                
-        far_scores = np.sort(far_scores)[::-1]
-        total_num_seconds = len(far_scores)
-        far_axis = (np.arange(total_num_seconds, dtype=float) + 1) / total_num_seconds
-                
-        # Find the indices where far_scores changes
-        unique_indices = np.concatenate(([0], np.where(np.diff(far_scores))[0] + 1))
-
-        downsampled_far_scores = far_scores[unique_indices]
-        downsampled_far_axis = far_axis[unique_indices]
-        
-        source = ColumnDataSource(
-            data=dict(x=downsampled_far_scores, y=downsampled_far_axis, name=[title]*len(downsampled_far_scores))
-        )
-        
-        line = p.line("x", "y", source=source, line_color=color, legend_label=title)
-
-    hover = HoverTool()
-    hover.tooltips = tooltips
-    p.add_tools(hover)
-    
-    p.legend.location = "top_right"
-    p.legend.click_policy = "hide"
-    p.legend.click_policy = "hide"
-
-    output_file(output_path, title="FAR Plot (Logarithmic)")
-    save(p)
-        
-def plot_efficiency_curves(model_names, model_titles, model_data, thresholds, output_path):
-    plot_width = 800
-    plot_height = 600
-    colors = cycle(['red', 'blue', 'green', 'orange'])
-
-    snr = np.linspace(0, 10, 41)
-    p = figure(
-        width=plot_width,
-        height=plot_height,
-        x_axis_label="SNR",
-        y_axis_label="Accuracy"
-    )
-
-    # Set the initial plot title
-    far_keys = list(thresholds[model_names[0]].keys())
-    p.title.text = f'FAR: {far_keys[0]}'
-
-    legend_items = []
-    all_sources = {}
-    acc_data = {}
-
-    for name, title, color in zip(model_names, model_titles, colors):
-        scores = model_data[name]["efficiency_scores"]
-        acc_all_fars = []
-
-        for far_index, far in enumerate(thresholds[model_names[0]].keys()):
-            threshold = thresholds[name][far][1]
-            actual_far = thresholds[name][far][0]
-            acc = []
-
-            for score in scores:
-                score = score[:, 1]
-                if threshold != 0:
-                    total = np.sum(score >= threshold)
-                else:
-                    total = np.sum(score > threshold)
-                                        
-                acc.append(total / len(score))
-            
-            acc_all_fars.append(acc)
-
-        acc_data[name] = acc_all_fars
-        source = ColumnDataSource(data=dict(x=snr, y=acc_all_fars[0], name=[title] * len(snr)))
-        all_sources[name] = source
-        line = p.line(x='x', y='y', source=source, line_width=1, line_color=color)
-        legend_items.append((title, [line]))
-
-    legend = Legend(items=legend_items, location="bottom_right")
-    p.add_layout(legend)
-    p.legend.click_policy = "hide"
-
-    hover = HoverTool()
-    hover.tooltips = [("Name", "@name"), ("SNR", "@x"), ("Accuracy", "@y")]
-    p.add_tools(hover)
-
-
-    slider = Slider(start=0, end=len(thresholds[model_names[0]].keys()) - 1, value=0, step=1, title=f"FAR Index: {far_keys[0]}")
-    slider.background = 'white'
-
-    callback = CustomJS(args=dict(slider=slider, sources=all_sources, plot_title=p.title, acc_data=acc_data, thresholds=thresholds, model_names=model_names, far_keys=far_keys), code="""
-        const far_index = slider.value;
-        const far_value = far_keys[far_index];
-        plot_title.text = 'FAR: ' + far_value;
-
-        for (const key in sources) {
-            if (sources.hasOwnProperty(key)) {
-                const source = sources[key];
-                source.data.y = acc_data[key][far_index];
-                source.change.emit();
-            }
-        }
-    """)             
-    slider.js_on_change('value', callback)
-
-    # Add a separate callback to update the slider's title
-    slider_title_callback = CustomJS(args=dict(slider=slider, far_keys=far_keys), code="""
-        const far_index = slider.value;
-        const far_value = far_keys[far_index];
-        slider.title = 'FAR Index: ' + far_value;
-    """)
-    slider.js_on_change('value', slider_title_callback)
-
-    layout = column(slider, p)
-    
-    output_file(output_path)
-    save(layout) 
-    
-def remove_redundant_points(x, y, epsilon):
-    if len(x) != len(y):
-        raise ValueError("Lengths of x and y arrays should be the same.")
-        
-    new_x, new_y = [x[0]], [y[0]]
-    
-    for i in range(1, len(x)):
-        if abs(x[i] - new_x[-1]) > epsilon or abs(y[i] - new_y[-1]) > epsilon:
-            new_x.append(x[i])
-            new_y.append(y[i])
-            
-    return new_x, new_y
-
-def plot_roc_curves(model_names, data, output_path, epsilon=1e-6):
-    p = figure(title="Receiver Operating Characteristic (ROC) Curves",
-               x_axis_label='False Positive Rate',
-               y_axis_label='True Positive Rate',
-               width=800, height=600,
-               x_axis_type='log', x_range=[1e-6, 1], y_range=[0, 1])
-
-    p.line([0, 1], [0, 1], color='navy', line_width=2, line_dash='dashed', legend_label="Random (area = 0.5)")
-
-    colors = cycle(['red', 'green', 'blue', 'purple', 'orange', 'brown', 'pink', 'cyan', 'magenta', 'yellow'])
-    
-    for color, model_name in zip(colors, model_names):
-        
-        roc_data = data[model_name]["roc_data"]
-
-        fpr = roc_data["fpr"]
-        tpr = roc_data["tpr"]
-        roc_auc = roc_data["roc_auc"]
-        
-        reduced_fpr, reduced_tpr = remove_redundant_points(fpr, tpr, epsilon)
-        source = ColumnDataSource(data=dict(x=reduced_fpr, y=reduced_tpr))
-        line = p.line(x='x', y='y', source=source, color=color, width=2, legend_label=f'{model_name} (area = {roc_auc:.5f})')
-        
-        hover = HoverTool(tooltips=[("Series", f"{model_name}"),
-                                     ("False Positive Rate", "$x{0.0000}"),
-                                     ("True Positive Rate", "$y{0.0000}")],
-                          renderers=[line])
-        p.add_tools(hover)
-
-    p.legend.location = "bottom_right"
-
-    output_file(output_path)
-    save(p)
+from py_ml_tools.validate import Validator, plot_efficiency_curves, plot_far_curves, plot_roc_curves
+from pathlib import Path
 
 if __name__ == "__main__":
-    restriction = 0
-    data_directory = "../skywarp_data/"
-   # Define model names and titles
+    
+    # User parameters:
+    data_directory = Path("../skywarp_data_0/")
+    fars = np.logspace(-1, -7, 500)
     model_names = [
         "skywarp_attention_regular", 
         "skywarp_conv_attention_regular", 
         "skywarp_conv_attention_single", 
         "skywarp_conv_regular"
     ]
-    model_titles = [
-        "Pure Attention Architecture", 
-        "Convolutional Attention Architecture", 
-        "Convolutional Single Attention Architecture", 
-        "Convolutional Architecture"
-    ]
-
-    # Define data types
-    data_types = [
-        ("real_noise", "Real Noise")
-    ]
-    
-    # Generate model names and titles for each data type
-    model_names = [f"{name}_{data_type[0]}" for name in model_names for data_type in data_types]
-    model_titles = [f"{title} {data_type[1]}" for title in model_titles for data_type in data_types]
-        
-    fars = np.logspace(-1, -7, 500)
     
     data = {}
-    for model in model_names:
-        roc_data, efficiency_scores, far_scores = load_data_from_hdf5(f"{data_directory}/{model}")
-        data[model] = {"roc_data": roc_data, "efficiency_scores" : efficiency_scores, "far_scores": far_scores}
+    validators = []
+    for model_name in model_names:
         
-    # Calculate thresholds
-    thresholds = calculate_far_score_thresholds(model_names, data, fars, restriction)
+        validators.append(
+            Validator.load(data_directory / f"{model_name}_validation_data.h5")
+        )
         
-    output_path = "../skywarp_data/efficiency_curves.html"
-    plot_efficiency_curves(model_names, model_titles, data, thresholds, output_path)
+    plot_efficiency_curves(
+        validators, 
+        fars, 
+        data_directory / "efficiency_curves.html"
+    )
     
-    output_path = "../skywarp_data/far_curves.html"
-    plot_far_curves(model_names, model_titles, data, fars, output_path)
+    plot_far_curves(
+        validators,
+        data_directory / "far_curves.html"
+    )
     
-    output_path = "../skywarp_data/roc_curves.html"
-    plot_roc_curves(model_names, data, output_path, epsilon=1e-6)
+    plot_roc_curves(
+        validators,
+        data_directory / "roc_curves.html"
+    )
--- validate_model.py
+++ validate_model.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-
 import numpy as np
 import logging
 
@@ -8,132 +7,43 @@ from tensorflow.data import Dataset
 from tensorflow.keras import mixed_precision, layers
 from tensorflow.keras import backend as K
 from tensorflow.data.experimental import AutoShardPolicy
-from functools import reduce
 
 from py_ml_tools.dataset  import get_ifo_data, O3, get_ifo_data_generator
 from py_ml_tools.setup    import setup_cuda, find_available_GPUs
-from py_ml_tools.validate import calculate_efficiency_scores
+from py_ml_tools.validate import Validator
 
-import matplotlib.pyplot as plt
-
-import h5py
-
-import gc
-
-from tqdm import tqdm
-from common_functions import *
-
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '0'
-
-def save_data_to_hdf5(model_name, noise_type, roc_data, efficiency_scores, far_scores):
-    with h5py.File(f'../skywarp_data/{model_name}_{noise_type}_data.h5', 'w') as h5f:
-        # Save ROC data
-        roc_group = h5f.create_group('roc_data')
-        roc_group.create_dataset('fpr', data=roc_data['fpr'])
-        roc_group.create_dataset('tpr', data=roc_data['tpr'])
-        roc_group.attrs['roc_auc'] = roc_data['roc_auc']
-
-        # Save efficiency scores
-        eff_group = h5f.create_group('efficiency_scores')
-        for i, score in enumerate(efficiency_scores):
-            eff_group.create_dataset(f'score_{i}', data=score)
-
-        # Save FAR scores
-        far_group = h5f.create_group('far_scores')
-        far_group.create_dataset('scores', data=far_scores)
-
-@tf.function
-def roc_curve_and_auc(y_true, y_scores, chunk_size=500):
-    num_thresholds = 1000
-     # Use logspace with a range between 0 and 6, which corresponds to values between 1 and 1e-6
-    log_thresholds = tf.exp(tf.linspace(0, -6, num_thresholds))
-    # Generate thresholds focusing on values close to 1
-    thresholds = 1 - log_thresholds
-    
-    thresholds = tf.cast(thresholds, tf.float32)
-    y_true = tf.cast(y_true, tf.float32)
-
-    num_samples = y_true.shape[0]
-    num_chunks = (num_samples + chunk_size - 1) // chunk_size
-
-    # Initialize accumulators for true positives, false positives, true negatives, and false negatives
-    tp_acc = tf.zeros(num_thresholds, dtype=tf.float32)
-    fp_acc = tf.zeros(num_thresholds, dtype=tf.float32)
-    fn_acc = tf.zeros(num_thresholds, dtype=tf.float32)
-    tn_acc = tf.zeros(num_thresholds, dtype=tf.float32)
-
-    # Process data in chunks
-    for chunk_idx in range(num_chunks):
-        start_idx = chunk_idx * chunk_size
-        end_idx = min((chunk_idx + 1) * chunk_size, num_samples)
-
-        y_true_chunk = y_true[start_idx:end_idx]
-        y_scores_chunk = y_scores[start_idx:end_idx]
-
-        y_pred = tf.expand_dims(y_scores_chunk, 1) >= thresholds
-        y_pred = tf.cast(y_pred, tf.float32)
-
-        y_true_chunk = tf.expand_dims(y_true_chunk, axis=-1)
-        tp = tf.reduce_sum(y_true_chunk * y_pred, axis=0)
-        fp = tf.reduce_sum((1 - y_true_chunk) * y_pred, axis=0)
-        fn = tf.reduce_sum(y_true_chunk * (1 - y_pred), axis=0)
-        tn = tf.reduce_sum((1 - y_true_chunk) * (1 - y_pred), axis=0)
-
-        # Update accumulators
-        tp_acc += tp
-        fp_acc += fp
-        fn_acc += fn
-        tn_acc += tn
-
-    tpr = tp_acc / (tp_acc + fn_acc)
-    fpr = fp_acc / (fp_acc + tn_acc)
-
-    auc = tf.reduce_sum((fpr[:-1] - fpr[1:]) * (tpr[:-1] + tpr[1:])) / 2
-
-    return fpr, tpr, auc
-
-def calculate_roc_data(model, dataset):
-    # Use .map() to extract the true labels and model inputs
-    x_dataset = dataset.map(lambda x, y: x)
-    y_true_dataset = dataset.map(lambda x, y: tf.cast(y['injection_masks'][0], tf.int32))
-        
-    # Convert the true labels dataset to a tensor using reduce
-    tensor_list = []
-    for batch in y_true_dataset:
-        tensor_list.append(batch)
-
-    y_true = tf.concat(tensor_list, axis=0)
-
-    # Get the model predictions
-    y_scores = model.predict(x_dataset, verbose = 1)[:, 1]
-
-    # Calculate the ROC curve and AUC
-    fpr, tpr, roc_auc = roc_curve_and_auc(y_true, y_scores)
-
-    return fpr.numpy(), tpr.numpy(), roc_auc.numpy()
-
-def calculate_far_scores(model, noise_ds, num_examples_per_batch, num_examples=1E5):
-    num_steps = int(num_examples // num_examples_per_batch)
-    noise_ds = noise_ds.take(num_steps)
-    
-    # Predict the scores and get the second column ([:, 1])
-    far_scores = model.predict(noise_ds, steps = num_steps, verbose=2)[:, 1]
-    
-    return far_scores
-                            
+from pathlib import Path
+            
 if __name__ == "__main__":
     
     # User parameters:
-    skywarp_data_directory       = "../skywarp_data_0/"
+    skywarp_data_directory       = Path("../skywarp_data_0/")
     num_examples_per_batch       = 32
-    num_far_tests                = int(1E4)
     sample_rate_hertz            = 2048.0
     max_segment_duration_seconds = 2048.0
     onsource_duration_seconds    = 1.0
     
-    data_directory_path = "./skywarp_data"
-    
+    efficiency_config = \
+        {
+            "max_snr" : 20.0, 
+            "num_snr_steps" : 41, 
+            "num_examples_per_snr_step" : 8192
+        }
+    far_config = \
+        {
+            "num_examples" : 1.0E6
+        }
+    roc_config : dict = \
+        {
+            "num_examples" : 1.0E6,
+            "snr_ranges" :  [
+                (8.0, 20.0),
+                8.0,
+                10.0,
+                12.0
+            ]
+        }
+        
     gpus = find_available_GPUs(10000, 1)
     strategy = setup_cuda(gpus, 8000, verbose = True)
     
@@ -147,8 +57,6 @@ if __name__ == "__main__":
     # Load datasets:
     policy = mixed_precision.Policy('mixed_float16')
     mixed_precision.set_global_policy(policy)
-    options = tf.data.Options()
-    options.experimental_distribute.auto_shard_policy = AutoShardPolicy.DATA
     
     with strategy.scope():
         
@@ -226,19 +134,17 @@ if __name__ == "__main__":
                         }
                 }
             }
-
-        injection_configs = [injection_config]
-
-        cbc_args = {
+        
+        generator_args = {
             "time_interval" : O3,
             "data_labels" : ["noise", "glitches"],
             "ifo" : "L1",
-            "injection_configs" : injection_configs,
+            "injection_configs" : [injection_config],
             "sample_rate_hertz" : sample_rate_hertz,
             "onsource_duration_seconds" : onsource_duration_seconds,
             "max_segment_size" : max_segment_duration_seconds,
             "num_examples_per_batch" : num_examples_per_batch,
-            "data_directory" : data_directory_path,
+            "data_directory" : skywarp_data_directory,
             "order" : "random",
             "seed" : 200,
             "apply_whitening" : True,
@@ -247,81 +153,26 @@ if __name__ == "__main__":
             "save_segment_data" : True
         }
         
-        cbc_ds = get_ifo_data_generator(
-            **cbc_args
-        ).with_options(options)
-        
-        balanced_args = cbc_args.copy()
-
-        balanced_config = injection_config.copy()
-        balanced_config.update({"injection_chance": 0.5})
-        balanced_args.update({
-            "injection_configs" : [balanced_config],
-        })
-        
-        balanced_dataset = get_ifo_data_generator(
-            **balanced_args
-        ).with_options(options)
-        
-        noise_args = cbc_args.copy()
-        
-        noise_args.update({
-            "injection_configs" : [],
-            "output_keys" : []
-        })
-            
-        noise_ds = get_ifo_data_generator(
-            **noise_args
-        ).with_options(options)
-        
         for model_name in model_names:
             
             logging.info(f"Loading model {model_name}...")
             model = tf.keras.models.load_model(
-                f"{skywarp_data_directory}/models/{model_name}"
+                skywarp_data_directory / f"models/{model_name}"
             )
             logging.info("Done.")
-        
-            roc_data = {}  
-
-            logging.info(f"Calculate {model_name} ROC data...")
-            
-            fpr, tpr, roc_auc = calculate_roc_data(
-                model, balanced_dataset.take(1000//num_examples_per_batch)
-            )
-            roc_data = {'fpr': fpr, 'tpr': tpr, 'roc_auc': roc_auc}
-            logging.info("Done.")
-            
-            logging.info(f"Calculating {model_name} efficiency scores...")
-            path_suffix = f"{skywarp_data_directory}/datasets/cbc"
-            efficiency_scores = \
-                calculate_efficiency_scores(
+                        
+            # Validate model:
+            validator = \
+                Validator.validate(
                     model, 
-                    cbc_args,
-                    32,
-                    10.0,
-                    41,
-                    8192
+                    model_name,
+                    generator_args,
+                    num_examples_per_batch,
+                    efficiency_config,
+                    far_config,
+                    roc_config
                 )
-            logging.info("Done.")
-
-            logging.info(f"Calculating {model_name} FAR scores...")
             
-            far_scores = \
-                calculate_far_scores(
-                    model, 
-                    noise_ds, 
-                    num_examples_per_batch, 
-                    num_examples=num_far_tests
-                )
-            logging.info("Done.")
-
-            logging.info(f"Saving {model_name} validation data...")
-            save_data_to_hdf5(
-                model_name, 
-                "real_noise", 
-                roc_data, 
-                efficiency_scores, 
-                far_scores
+            validator.save(
+                skywarp_data_directory / f"{model_name}_validation_data.h5", 
             )
-            logging.info("Done.")
